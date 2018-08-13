a388c5a01e77b1efcd9bad573776af55f2dbf14d
Dan Passaro <danpassaro@gmail.com>
2018-08-13 09:07:30 -0400

    Move test to correct file

--- test/controllers/weight_entries_controller_test.rb
+++ test/controllers/weight_entries_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class WeightEntriesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get create" do
-  #   post weight_entries_create_url, {weight_entry: {value: '120'}}
-  #   assert_response :success
-  # end
+  test "associates weight entries with the current user" do
+    user = User.create(email: 'auser@example.com', password: 'password')
+    sign_in user
 
+    post weight_entries_url, {weight_entry: {value: 150}}
+
+    assert_equal 1, user.weight_entries.count
+    new_entry = user.weight_entries.first
+    assert_equal 150, new_entry.value
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -47,15 +47,4 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_select 'h2', '140 lbs'
   end
-
-  test "associates weight entries with the correct user" do
-    user = User.create(email: 'auser@example.com', password: 'password')
-    sign_in user
-
-    post weight_entries_url, {weight_entry: {value: 150}}
-
-    assert_equal 1, user.weight_entries.count
-    new_entry = user.weight_entries.first
-    assert_equal 150, new_entry.value
-  end
 end
