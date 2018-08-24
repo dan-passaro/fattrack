fc94578944f71084891348fa3a2eced379df000f
Dan Passaro <danpassaro@gmail.com>
2018-08-24 18:40:22 -0400

    Cleanup nav slightly

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,8 @@ class WelcomeController < ApplicationController
   def index
     @todays_weight = WeightEntry.find_by(day: Date.current, user: current_user)
 
+    @has_old_entries = WeightEntry.where(user: current_user).exists
+
     # If they need to put one in
     @weight_entry = WeightEntry.new
   end
