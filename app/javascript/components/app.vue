959175e109f3c66ca3f1ca1876e932dc3d7f62e8
Dan Passaro <danpassaro@gmail.com>
2018-09-19 06:55:54 -0400

    Link to older entries

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,48 +1,51 @@
 <template>
-  <div class="row">
-    <div class="text-center col-xs-12 col-md-6 col-md-offset-3">
-      <h2 class="h3">Your weight for today:</h2>
+  <div>
+    <div class="row">
+      <div class="text-center col-xs-12 col-md-6 col-md-offset-3">
+        <h2 class="h3">Your weight for today:</h2>
 
-      <div
-        v-if="editing"
-        key="enter-value"
-      >
-        <form @submit.prevent="update">
-          <div class="text-center col-xs-12 col-md-6 col-md-offset-3">
-            <p>
-              <input
-                class="form-control"
-                name="fizz"
-                v-model="entry.value"
-              >
-            </p>
-          </div>
+        <div
+          v-if="editing"
+          key="enter-value"
+        >
+          <form @submit.prevent="update">
+            <div class="text-center col-xs-12 col-md-6 col-md-offset-3">
+              <p>
+                <input
+                  class="form-control"
+                  name="fizz"
+                  v-model="entry.value"
+                >
+              </p>
+            </div>
 
-          <div class="text-center col-xs-12 col-md-6 col-md-offset-3">
-            <p>
-              <input
-                type="submit"
-                class="btn btn-primary"
-                value="Submit"
-              >
-            </p>
-          </div>
-        </form>
-      </div>
+            <div class="text-center col-xs-12 col-md-6 col-md-offset-3">
+              <p>
+                <input
+                  type="submit"
+                  class="btn btn-primary"
+                  value="Submit"
+                >
+              </p>
+            </div>
+          </form>
+        </div>
 
-      <div
-        v-else
-        class="text-center col-xs-12 col-md-6 col-md-offset-3"
-        key="show-value"
-      >
-        <h2 class="h3">
-          {{ entry.value }} lbs
-          <a href="#" @click="editEntryBegin">
-            <span class="glyphicon glyphicon-pencil"></span>
-          </a>
-        </h2>
+        <div
+          v-else
+          class="text-center col-xs-12 col-md-6 col-md-offset-3"
+          key="show-value"
+        >
+          <h2 class="h3">
+            {{ entry.value }} lbs
+            <a href="#" @click="editEntryBegin">
+              <span class="glyphicon glyphicon-pencil"></span>
+            </a>
+          </h2>
+        </div>
       </div>
     </div>
+    <p><a href="/weight_entries">See old entries</a></p>
   </div>
 </template>
 
